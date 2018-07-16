029c0d7ca36e698ca4daf4905347e3ba9ae64422
jiangjiwei <jiangjiwei0322@outlook.com>
2018-07-16 15:12:57 +0800

    1. 新增 AvatarView

--- app/src/main/java/me/monster/hencoderplusview/view/RoundAvatar.java
+++ app/src/main/java/me/monster/hencoderplusview/view/RoundAvatar.java
@@ -1,9 +1,16 @@
 package me.monster.hencoderplusview.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import me.monster.hencoderplusview.R;
+import me.monster.hencoderplusview.util.ValueUtil;
 
 /**
  * @description 圆形头像 带边框
@@ -11,6 +18,11 @@ import android.view.View;
  * Created in 2018/7/16 9:13
  */
 public class RoundAvatar extends View {
+
+    private Bitmap avatarBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.wx_avatat);
+    private float padding = ValueUtil.dpToPixel(8);
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
 
     public RoundAvatar(Context context) {
         super(context);
@@ -26,5 +38,12 @@ public class RoundAvatar extends View {
 
     public RoundAvatar(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(avatarBitmap, padding, padding, mPaint);
     }
 }
