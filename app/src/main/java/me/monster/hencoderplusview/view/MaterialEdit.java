fc58fb51c7a6f35370ee474d8059e2cf6e9d8d3d
jiangjiwei <jiangjiwei0322@outlook.com>
2018-07-24 10:18:29 +0800

    1. 根据当前 EditText 是否获取到焦点进行动画

--- app/src/main/java/me/monster/hencoderplusview/view/MaterialEdit.java
+++ app/src/main/java/me/monster/hencoderplusview/view/MaterialEdit.java
@@ -6,11 +6,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
-
-import java.util.Map;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 import me.monster.hencoderplusview.R;
 import me.monster.hencoderplusview.util.ValueUtil;
@@ -21,6 +19,7 @@ import me.monster.hencoderplusview.util.ValueUtil;
  * @date 2018-07-23 7:43
  */
 public class MaterialEdit extends android.support.v7.widget.AppCompatEditText {
+    private static final String TAG = "MaterialEdit";
 
     private static float LABEL_SPACE = ValueUtil.dpToPixel(8);
     private static float LABEL_SIZE = ValueUtil.dpToPixel(14);
@@ -31,39 +30,56 @@ public class MaterialEdit extends android.support.v7.widget.AppCompatEditText {
 
     private int labelColor;
 
-    private ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(this, "labelAlpha", 1);
+    private ObjectAnimator alphaAnimator;
 
     private boolean showLabel = false;
 
     private float labelAlpha = 0;
+
+    private String labelText = "";
+
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     {
         setPadding(getPaddingLeft(), (int) (getPaddingTop() + LABEL_PADDING + LABEL_SIZE), getPaddingRight(), getPaddingBottom());
         mPaint.setTextSize(LABEL_SIZE);
 
-        this.addTextChangedListener(new TextWatcher() {
+        this.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0 && !showLabel) {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
                     showLabel = true;
-                    alphaAnimator.start();
-                } else if (showLabel && s.length() <= 0) {
+                    getAnimator().start();
+                } else {
                     showLabel = false;
-                    alphaAnimator.reverse();
+                    MaterialEdit.this.setHint(labelText);
+                    getAnimator().reverse();
                 }
             }
         });
+
+//        this.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                if (s.length() > 0 && !showLabel) {
+//                    showLabel = true;
+//                    getAnimator().start();
+//                } else if (showLabel && s.length() <= 0) {
+//                    showLabel = false;
+//                    getAnimator().reverse();
+//                }
+//            }
+//        });
     }
 
     public MaterialEdit(Context context) {
@@ -73,7 +89,14 @@ public class MaterialEdit extends android.support.v7.widget.AppCompatEditText {
     @RequiresApi(api = Build.VERSION_CODES.M)
     public MaterialEdit(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLabelColor(context.getColor(R.color.colorAccent));;
+
+        for (int i = 0; i < attrs.getAttributeCount(); i++) {
+            if ("hint".equals(attrs.getAttributeName(i))) {
+                labelText = attrs.getAttributeValue(i);
+                break;
+            }
+        }
+        setLabelColor(context.getColor(R.color.colorAccent));
     }
 
     public float getLabelAlpha() {
@@ -91,6 +114,7 @@ public class MaterialEdit extends android.support.v7.widget.AppCompatEditText {
 
     /**
      * Color id
+     *
      * @param labelColor labelColor
      */
     public void setLabelColor(int labelColor) {
@@ -101,17 +125,23 @@ public class MaterialEdit extends android.support.v7.widget.AppCompatEditText {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (getText().length() != 0) {
+        if (showLabel) {
             mPaint.setAlpha((int) (labelAlpha * 0xff));
             mPaint.setColor(labelColor);
             float moveLength = (1 - labelAlpha) * LABEL_OFFSET_Y;
-            canvas.drawText(getHint().toString(), LABEL_OFFSET, LABEL_PADDING + LABEL_SIZE + moveLength, mPaint);
+            canvas.drawText(labelText, LABEL_OFFSET, LABEL_PADDING + LABEL_SIZE + moveLength, mPaint);
             mPaint.setAlpha(1);
+            setHint("");
         }
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    private ObjectAnimator getAnimator() {
+        if (alphaAnimator == null) {
+            alphaAnimator = ObjectAnimator.ofFloat(this, "labelAlpha", (float) 0.9);
+            alphaAnimator.setInterpolator(new LinearInterpolator());
+            alphaAnimator.setDuration(200);
+        }
+        return alphaAnimator;
     }
 }
